cff61cd2e2996874314adf5adc501face66e1153
bienv-007 <bienvvasima@gmail.com>
2026-05-09 22:57:56 +0200

    creation de la chaine de connexion

---
+++ connexion_db/connexion.php
@@ -0,0 +1,7 @@
+<?php 
+try {
+    // notre chaine de connexion
+    $connexion = new Pdo("mysql:host=localhost;dbname=db_reservation_restau","root","");
+} catch (PDOException $e) {
+    die("erreur : ".$e->getMessage());
+}
