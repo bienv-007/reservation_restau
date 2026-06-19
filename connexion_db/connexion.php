d668efdc60dca1b6aa7e85b57cd969bc3dfdaee8
bienv-007 <bienvvasima@gmail.com>
2026-06-19 09:32:00 +0200

    arrangement de la chaine de connexion

--- connexion_db/connexion.php
+++ connexion_db/connexion.php
@@ -1,7 +1,7 @@
 <?php 
 try {
     // notre chaine de connexion
-    $connexion = new Pdo("mysql:host=localhost;dbname=db_reservation_restau","root","");
+    $connexion = new Pdo("mysql:host=localhost;dbname=db_reservation_restau","root",""); // chaine de connexion
 } catch (PDOException $e) {
     die("erreur : ".$e->getMessage());
 }
